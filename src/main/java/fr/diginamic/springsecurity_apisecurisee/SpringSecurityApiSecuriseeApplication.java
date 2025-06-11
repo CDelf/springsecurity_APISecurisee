1dfefe23f283db220eaee3a5e48305ee865a0820
Carole <c.delfino@live.fr>
2025-06-11 15:18:11 +0200

    Commit initial

---
+++ src/main/java/fr/diginamic/springsecurity_apisecurisee/SpringSecurityApiSecuriseeApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.springsecurity_apisecurisee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApiSecuriseeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApiSecuriseeApplication.class, args);
+    }
+
+}
